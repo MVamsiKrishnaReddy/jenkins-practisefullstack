55fd0da2e02ffe3c42b726f4f0f6da8f537eea4c
MVK <2300031063@kluniversity.in>
2025-09-05 21:37:48 +0530

    React App V1.0

---
+++ BOOKAPI-SPRINGBOOT/src/main/java/com/klef/dev/BookapiSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.klef.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookapiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookapiSpringbootApplication.class, args);
+		System.out.println("Project is running");
+	}
+
+}
---
+++ BOOKAPI-SPRINGBOOT/src/main/java/com/klef/dev/model/Book.java
@@ -0,0 +1,105 @@
+package com.klef.dev.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "book_table")
+public class Book {
+    
+    @Id
+    @Column(name = "book_id")
+    private int id;
+    
+    @Column(name = "book_title", nullable = false, length = 100)
+    private String title;
+    
+    @Column(name = "book_author", nullable = false, length = 50)
+    private String author;
+    
+    @Column(name = "book_genre", nullable = false, length = 30)
+    private String genre;
+    
+    @Column(name = "book_publisher", nullable = false, length = 50)
+    private String publisher;
+    
+    @Column(name = "book_year", nullable = false)
+    private int year;
+    
+    @Column(name = "book_isbn", nullable = false, unique = true, length = 20)
+    private String isbn;
+    
+    @Column(name = "book_price", nullable = false)
+    private double price;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    public String getPublisher() {
+        return publisher;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString() {
+        return "Book [id=" + id 
+            + ", title=" + title 
+            + ", author=" + author 
+            + ", genre=" + genre 
+            + ", publisher=" + publisher 
+            + ", year=" + year 
+            + ", isbn=" + isbn 
+            + ", price=" + price + "]";
+    }
+}
